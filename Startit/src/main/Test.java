d22b688ff2238fdd1d57a593f1a3a39921398f2d
Z003RNYJ <Z003RNYJ@AAEINBLR03598L.ad001.siemens.net>
2017-07-12 23:24:57 +0530

    Added

---
+++ Startit/src/main/Test.java
@@ -0,0 +1,17 @@
+package com.ankit.maven.Startit;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+	    System.out.println("Enter the String & Number :");
+	    String name = in.nextLine();
+	    int numbers = in.nextInt();
+	    System.out.println("Name :"+ name + "Number :" +numbers);
+	}
+
+}
+
---
+++ Startit/src/main/java/com/ankit/maven/Startit/App.java
@@ -0,0 +1,13 @@
+package com.ankit.maven.Startit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
